25c5121d4dde29e7f6288403d685a7f8743a6094
Nikhil Kansal <nkansal96@gmail.com>
2018-11-13 13:59:38 -0800

    update routing

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -9,29 +9,29 @@ import {render} from 'react-dom';
 
 import {store, history} from 'reducers';
 
-import Home from 'pages/home';
-import CreateTrip from 'pages/createTrip';
 import CreateEvent from 'pages/createEvent';
+import CreateTrip from 'pages/createTrip';
 import ViewEvent from 'pages/ViewEvent';
 import ViewTrip from 'pages/ViewTrip';
+import ViewTrips from 'pages/home';
 
 class App extends React.Component {
-	render(){
-		return (
-			<Provider store={store}>
-				<ConnectedRouter history={history}>
-					<Switch>
-						<Route exact path="/trips" component={Home}/>
-						<Route path="/trips/create" component={CreateTrip} />
-						<Route path="/event/create" component={CreateEvent}/>
-						<Route path="/events" component={ViewEvent} />
-						<Route path="/trips/:id" component={route => <ViewTrip route={route} />} />
-						<Redirect to="/trips"/>
-					</Switch>
-				</ConnectedRouter>
-			</Provider>
-		);
-	}
+  render(){
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route exact path="/trips" component={ViewTrips}/>
+            <Route exact path="/trips/create" component={CreateTrip} />
+            <Route path="/trips/:id" component={({match}) => <ViewTrip id={match.params.id} />} />
+            <Route exact path="/trips/:id/createEvent" component={({match}) => <CreateEvent tripId={match.params.id} />} />
+            <Route path="/trips/:id/:event" component={({match}) => <ViewEvent tripId={match.params.id} eventId={match.params.event} />} />
+            <Redirect to="/trips"/>
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
 }
 
 render(<App />, document.getElementById('root'));

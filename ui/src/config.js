81337b0f8cb18b9570c121bac31faf4fda6c9bff
Nikhil Kansal <nkansal96@gmail.com>
2018-11-12 18:53:52 -0800

    tabs to spaces

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,13 +1,13 @@
 const API_URL = process.env.WEBPACK ? 'http://localhost:3000' : '';
 
 export default {
-	routes: {
-		trips: {
-			get: () => `${API_URL}/api/trips`,
-			getOne: id => `${API_URL}/api/trips/${id}`,
-			update: id => `${API_URL}/api/trips/${id}`,
-			delete: id => `${API_URL}/api/trips/${id}`,
-			create: () => `${API_URL}/api/trips`,
-		}
-	}
+  routes: {
+    trips: {
+      get: () => `${API_URL}/api/trips`,
+      getOne: id => `${API_URL}/api/trips/${id}`,
+      update: id => `${API_URL}/api/trips/${id}`,
+      delete: id => `${API_URL}/api/trips/${id}`,
+      create: () => `${API_URL}/api/trips`,
+    }
+  }
 };

6ffc7f1c1e77c4abd881707d1cfbcce81e00ab3b
Nikhil Kansal <nkansal96@gmail.com>
2018-11-28 00:00:53 -0800

    overlay login instead of redirecting

--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -7,9 +7,9 @@ import { LoginUser, CreateUser } from 'reducers/users';
 import TabController from 'components/tabs/TabController';
 import TextInput from 'components/input/Text';
 import PasswordInput from 'components/input/Password';
-import Section from 'components/section/Section';
 import Button from 'components/button/Button';
 import Header from 'components/header';
+import Section from 'components/section';
 
 class Login extends React.Component {
   constructor(props) {
@@ -20,25 +20,11 @@ class Login extends React.Component {
     };
   }
 
-  componentWillMount() {
-    if (this.props.authenticated) {
-      this.props.redirectHome();
-    }
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    if (nextProps.authenticated) {
-      this.props.redirectHome();
-    }
-  }
-
-  handleTabUpdate = i => {
+  handleTabUpdate = i =>
     this.setState(prev => ({...prev, logInView: i === 0}));
-  }
 
-  handleChange = (name, value) => {
+  handleChange = (name, value) =>
     this.setState(prev => ({...prev, user: {...prev.user, [name]: value }}));
-  }
 
   logIn = () => this.props.loginUser(this.state.user.email, this.state.user.password);
   signUp = () => this.props.createUser(this.state.user.name, this.state.user.email, this.state.user.password);
@@ -52,7 +38,7 @@ class Login extends React.Component {
         <Section title="Password">
           <PasswordInput name="password" onChange={this.handleChange} />
         </Section>
-        <Section title="">
+        <Section>
           <Button blue label="Log In" onClick={this.logIn} />
         </Section>
       </div>
@@ -71,7 +57,7 @@ class Login extends React.Component {
         <Section title="Password">
           <PasswordInput name="password" onChange={this.handleChange} />
         </Section>
-        <Section title="">
+        <Section>
           <Button blue label="Sign Up" onClick={this.signUp} />
         </Section>
       </div>
@@ -86,9 +72,7 @@ class Login extends React.Component {
           <TabController tabs={["Log In", "Sign Up"]} onChange={this.handleTabUpdate} />
           { this.state.logInView ? this.getLoginView() : this.getSignUpView() }
           { ((this.props.logInFailure || this.props.createUserFailure) && this.props.error) && 
-            <Section title="">
-              <div className="error">{ this.props.error }</div>
-            </Section>
+            <Section><div className="error">{ this.props.error }</div></Section>
           }
         </div>
       </div>

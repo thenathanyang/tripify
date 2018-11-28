efb691cbc4ef4518a05756a527d2e64e8b514f81
Nikhil Kansal <nkansal96@gmail.com>
2018-11-27 23:57:54 -0800

    update routes to use trip-id-access only if possible

--- ui/src/config.js
+++ ui/src/config.js
@@ -6,11 +6,11 @@ export default {
       login: () => `${API_URL}/api/auth/login`,
     },
     trips: {
-      get:    (user)     => `${API_URL}/api/users/${user}/trips`,
-      getOne: (user, id) => `${API_URL}/api/users/${user}/trips/${id}`,
-      update: (user, id) => `${API_URL}/api/users/${user}/trips/${id}`,
+      get:    (user) => `${API_URL}/api/users/${user}/trips`,
+      getOne: (id)   => `${API_URL}/api/trips/${id}`,
+      create: (user) => `${API_URL}/api/users/${user}/trips`,
+      update: (id)   => `${API_URL}/api/trips/${id}`,
       delete: (user, id) => `${API_URL}/api/users/${user}/trips/${id}`,
-      create: (user)     => `${API_URL}/api/users/${user}/trips`,
     },
     users: {
       get:    () => `${API_URL}/api/users`,

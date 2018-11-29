9952c72b550232f58fe5748390ef80d9d7937310
Nikhil Kansal <nkansal96@gmail.com>
2018-11-28 18:14:51 -0800

    rename tam import to correct case

--- ui/src/components/tabs/TabController.jsx
+++ ui/src/components/tabs/TabController.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tab from './tab';
+import Tab from './Tab';
 
 export default class TabController extends React.Component {
   constructor(props) {

12d1c62bf22d2fcb03c13c065e97b9772e750d99
Nathan Yang <nathanyang@engineering.ucla.edu>
2018-11-29 17:46:07 -0800

    Refactor navbar to use only icons

--- ui/src/components/icon/index.jsx
+++ ui/src/components/icon/index.jsx
@@ -35,7 +35,7 @@ class Icon extends React.Component {
       [`${color}`]: true,
       [`fa-${icon}`]: true,
     };
-    return <span id={id} className={classnames(classes)} onClick={this.handleClick}/>;
+    return <span id={id} className={classnames(classes)} />;
   }
 }
 

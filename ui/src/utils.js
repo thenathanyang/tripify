4ab00bb4ca6b01849628bc3d754fe5eed786d9b6
Nikhil Kansal <nkansal96@gmail.com>
2018-12-03 16:59:23 -0800

    clean up reducers, add Array helper functions

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -2,3 +2,19 @@ export const classnames = classes =>
   Object.keys(classes)
     .filter(k => !!classes[k])
     .join(" ");
+
+Array.prototype.last = function() {
+  return this[this.length - 1];
+};
+
+Array.prototype.flatMap = function(fn) {
+  return this.map(fn).flatten();
+}
+
+Array.prototype.flatten = function() {
+  return this.reduce((acc, curr) => {
+    if (Array.isArray(curr))
+      return acc.concat(curr.flatten());
+    return acc.concat([curr]);
+  }, []);
+};
